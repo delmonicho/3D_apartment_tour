aa506c7d172eb8e41e327eb5fa0a365b4188f6f2
Nicho Del Moral <ndelmoral13@gmail.com>
2023-09-08 20:41:35 -0400

    add first person movement and camera input controls

---
+++ immersive_3D_env/Assets/Scripts/FPMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FPMovement : MonoBehaviour
+{
+    public Vector3 direction;
+    public float speed;
+
+    public Rigidbody rb;    // The player's rigidbody
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    // FixedUpdate is called at fixed intervals
+    // All physics calculations happen in FixedUpdate
+    void FixedUpdate()
+    {
+        // transform.Translate(direction * speed * Time.deltaTime);    
+        // need to configure movement to localDirection as opposed to default world direction
+
+        Vector3 localDirection = transform.TransformDirection(direction);
+        // move using physics
+        rb.MovePosition(rb.position + (localDirection * speed * Time.deltaTime));
+    }
+
+    // OnPlayerMove event handler
+    public void OnPlayerMove(InputValue value)
+    {
+        // A vector with x and y components corresponding to the player's WASD and arrow inputs
+        // both components are in the range [-1, 1]
+        Vector2 inputVector = value.Get<Vector2>();
+
+        // move in the XZ-plane
+        direction.x = inputVector.x;
+        direction.z = inputVector.y;
+
+    }
+}
